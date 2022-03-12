141372fd54c5f3ab38dbc9b7d4567895742d2908
shiba inu <jefferson121996@gmail.com>
2022-03-12 16:10:55 -0500

    declared variable to be used

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var results = "";
+  var characters = [];
+  var passwordLength = window.prompt("What is the length of the password you want to generate (min 8, max 128)");
+  var quantityCharacter = parseInt(passwordLength);
+  
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
